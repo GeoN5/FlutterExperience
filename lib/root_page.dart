34f472e5e750ecbaa96f22653fd82718f34daf6d
GeoN5 <sgepyh2916@naver.com>
2019-03-05 16:58:25 +0900

    screen design & draft write

---
+++ lib/root_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram/tab_page.dart';
+
+//로그인 했는지 안했는지 체크
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabPage();
+  }
+}
+
